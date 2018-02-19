79e8e5083852c9ffb2191d240e0c97204029c623
Charles Ma <charlesma.vip@gmail.com>
2018-02-19 19:33:44 +1100

    增加全局异常处理

--- DnaMes/DnaMesUi/Program.cs
+++ DnaMes/DnaMesUi/Program.cs
@@ -6,7 +6,9 @@
 // ****************************************************
 
 using System;
+using System.Threading;
 using System.Windows.Forms;
+using DnaLib.Helper;
 
 namespace DnaMesUi
 {
@@ -18,9 +20,33 @@ namespace DnaMesUi
         [STAThread]
         private static void Main()
         {
+            #region 全局异常处理
+            //TODO:项目完结后启用
+            ////设置应用程序处理异常方式：ThreadException处理  
+            //Application.SetUnhandledExceptionMode(UnhandledExceptionMode.CatchException);
+            ////处理UI线程异常  
+            //Application.ThreadException += Application_ThreadException;
+            ////处理非UI线程异常  
+            //AppDomain.CurrentDomain.UnhandledException += CurrentDomain_UnhandledException;
+
+            #endregion
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
+
             Application.Run(new Form1());
+        }
+        private static void CurrentDomain_UnhandledException(object sender, UnhandledExceptionEventArgs e)
+        {
+            var ex = (Exception)e.ExceptionObject;
+            MessageBoxHelper.ShowError(ex.Message);
+            LogHelper.WriteErrorLog(ex, "non-UI Exception", $"Runtime terminating: {e.IsTerminating}");
+        }
+
+        private static void Application_ThreadException(object sender, ThreadExceptionEventArgs e)
+        {
+            var ex = e.Exception;
+            MessageBoxHelper.ShowError(ex.Message);
+            LogHelper.WriteErrorLog(ex,"UI Exception");
         }
     }
 }
--- DnaMes/DnaMesUi/Shared/Dialog/MESLoginDialog.Designer.cs
+++ DnaMes/DnaMesUi/Shared/Dialog/MESLoginDialog.Designer.cs
@@ -32,7 +32,6 @@
             this.checkBoxStaffnoLogin = new System.Windows.Forms.CheckBox();
             this.lbName = new System.Windows.Forms.Label();
             this.txtBoxUserName = new System.Windows.Forms.TextBox();
-            this.lbStaffNo = new System.Windows.Forms.Label();
             this.txtStaffno = new System.Windows.Forms.TextBox();
             this.pdcLbPswd = new System.Windows.Forms.Label();
             this.txtBoxPassword = new System.Windows.Forms.TextBox();
@@ -42,15 +41,17 @@
             this.btnSaveIP = new System.Windows.Forms.Button();
             this.btnLogin = new System.Windows.Forms.Button();
             this.btnExit = new System.Windows.Forms.Button();
+            this.lbStaffNo = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // checkBoxStaffnoLogin
             // 
             this.checkBoxStaffnoLogin.AutoSize = true;
             this.checkBoxStaffnoLogin.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
-            this.checkBoxStaffnoLogin.Location = new System.Drawing.Point(323, 39);
+            this.checkBoxStaffnoLogin.Location = new System.Drawing.Point(431, 49);
+            this.checkBoxStaffnoLogin.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.checkBoxStaffnoLogin.Name = "checkBoxStaffnoLogin";
-            this.checkBoxStaffnoLogin.Size = new System.Drawing.Size(93, 25);
+            this.checkBoxStaffnoLogin.Size = new System.Drawing.Size(114, 31);
             this.checkBoxStaffnoLogin.TabIndex = 14;
             this.checkBoxStaffnoLogin.Text = "工号登入";
             this.checkBoxStaffnoLogin.UseVisualStyleBackColor = true;
@@ -61,37 +62,29 @@
             // 
             this.lbName.AutoSize = true;
             this.lbName.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
-            this.lbName.Location = new System.Drawing.Point(37, 37);
+            this.lbName.Location = new System.Drawing.Point(49, 46);
+            this.lbName.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lbName.Name = "lbName";
-            this.lbName.Size = new System.Drawing.Size(58, 21);
+            this.lbName.Size = new System.Drawing.Size(72, 27);
             this.lbName.TabIndex = 15;
             this.lbName.Text = "用户名";
             // 
             // txtBoxUserName
             // 
             this.txtBoxUserName.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
-            this.txtBoxUserName.Location = new System.Drawing.Point(121, 37);
+            this.txtBoxUserName.Location = new System.Drawing.Point(161, 46);
+            this.txtBoxUserName.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.txtBoxUserName.Name = "txtBoxUserName";
-            this.txtBoxUserName.Size = new System.Drawing.Size(167, 29);
+            this.txtBoxUserName.Size = new System.Drawing.Size(221, 34);
             this.txtBoxUserName.TabIndex = 1;
-            // 
-            // lbStaffNo
-            // 
-            this.lbStaffNo.AutoSize = true;
-            this.lbStaffNo.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
-            this.lbStaffNo.Location = new System.Drawing.Point(41, 37);
-            this.lbStaffNo.Name = "lbStaffNo";
-            this.lbStaffNo.Size = new System.Drawing.Size(42, 21);
-            this.lbStaffNo.TabIndex = 17;
-            this.lbStaffNo.Text = "工号";
-            this.lbStaffNo.Visible = false;
             // 
             // txtStaffno
             // 
             this.txtStaffno.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
-            this.txtStaffno.Location = new System.Drawing.Point(121, 37);
+            this.txtStaffno.Location = new System.Drawing.Point(161, 46);
+            this.txtStaffno.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.txtStaffno.Name = "txtStaffno";
-            this.txtStaffno.Size = new System.Drawing.Size(167, 29);
+            this.txtStaffno.Size = new System.Drawing.Size(221, 34);
             this.txtStaffno.TabIndex = 18;
             this.txtStaffno.Visible = false;
             // 
@@ -99,18 +92,20 @@
             // 
             this.pdcLbPswd.AutoSize = true;
             this.pdcLbPswd.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
-            this.pdcLbPswd.Location = new System.Drawing.Point(41, 101);
+            this.pdcLbPswd.Location = new System.Drawing.Point(55, 126);
+            this.pdcLbPswd.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.pdcLbPswd.Name = "pdcLbPswd";
-            this.pdcLbPswd.Size = new System.Drawing.Size(42, 21);
+            this.pdcLbPswd.Size = new System.Drawing.Size(52, 27);
             this.pdcLbPswd.TabIndex = 19;
             this.pdcLbPswd.Text = "密码";
             // 
             // txtBoxPassword
             // 
             this.txtBoxPassword.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
-            this.txtBoxPassword.Location = new System.Drawing.Point(121, 98);
+            this.txtBoxPassword.Location = new System.Drawing.Point(161, 122);
+            this.txtBoxPassword.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.txtBoxPassword.Name = "txtBoxPassword";
-            this.txtBoxPassword.Size = new System.Drawing.Size(167, 29);
+            this.txtBoxPassword.Size = new System.Drawing.Size(221, 34);
             this.txtBoxPassword.TabIndex = 2;
             this.txtBoxPassword.UseSystemPasswordChar = true;
             // 
@@ -118,9 +113,10 @@
             // 
             this.pdcLabelIP.AutoSize = true;
             this.pdcLabelIP.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
-            this.pdcLabelIP.Location = new System.Drawing.Point(37, 153);
+            this.pdcLabelIP.Location = new System.Drawing.Point(49, 191);
+            this.pdcLabelIP.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.pdcLabelIP.Name = "pdcLabelIP";
-            this.pdcLabelIP.Size = new System.Drawing.Size(58, 21);
+            this.pdcLabelIP.Size = new System.Drawing.Size(72, 27);
             this.pdcLabelIP.TabIndex = 21;
             this.pdcLabelIP.Text = "服务器";
             this.pdcLabelIP.Visible = false;
@@ -128,18 +124,20 @@
             // txtMESServerIP
             // 
             this.txtMESServerIP.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
-            this.txtMESServerIP.Location = new System.Drawing.Point(121, 150);
+            this.txtMESServerIP.Location = new System.Drawing.Point(161, 188);
+            this.txtMESServerIP.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.txtMESServerIP.Name = "txtMESServerIP";
-            this.txtMESServerIP.Size = new System.Drawing.Size(167, 29);
+            this.txtMESServerIP.Size = new System.Drawing.Size(221, 34);
             this.txtMESServerIP.TabIndex = 4;
             this.txtMESServerIP.Visible = false;
             // 
             // BtnchangeServerIP
             // 
             this.BtnchangeServerIP.Image = ((System.Drawing.Image)(resources.GetObject("BtnchangeServerIP.Image")));
-            this.BtnchangeServerIP.Location = new System.Drawing.Point(344, 100);
+            this.BtnchangeServerIP.Location = new System.Drawing.Point(459, 125);
+            this.BtnchangeServerIP.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.BtnchangeServerIP.Name = "BtnchangeServerIP";
-            this.BtnchangeServerIP.Size = new System.Drawing.Size(33, 29);
+            this.BtnchangeServerIP.Size = new System.Drawing.Size(44, 36);
             this.BtnchangeServerIP.TabIndex = 3;
             this.BtnchangeServerIP.UseVisualStyleBackColor = true;
             this.BtnchangeServerIP.Click += new System.EventHandler(this.BtnchangeServerIP_Click);
@@ -147,9 +145,10 @@
             // btnSaveIP
             // 
             this.btnSaveIP.Image = ((System.Drawing.Image)(resources.GetObject("btnSaveIP.Image")));
-            this.btnSaveIP.Location = new System.Drawing.Point(344, 150);
+            this.btnSaveIP.Location = new System.Drawing.Point(459, 188);
+            this.btnSaveIP.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btnSaveIP.Name = "btnSaveIP";
-            this.btnSaveIP.Size = new System.Drawing.Size(33, 33);
+            this.btnSaveIP.Size = new System.Drawing.Size(44, 41);
             this.btnSaveIP.TabIndex = 24;
             this.btnSaveIP.UseVisualStyleBackColor = true;
             this.btnSaveIP.Visible = false;
@@ -160,11 +159,12 @@
             this.btnLogin.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
             this.btnLogin.Image = ((System.Drawing.Image)(resources.GetObject("btnLogin.Image")));
             this.btnLogin.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.btnLogin.Location = new System.Drawing.Point(87, 204);
+            this.btnLogin.Location = new System.Drawing.Point(116, 255);
+            this.btnLogin.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btnLogin.Name = "btnLogin";
-            this.btnLogin.Size = new System.Drawing.Size(90, 32);
+            this.btnLogin.Size = new System.Drawing.Size(120, 40);
             this.btnLogin.TabIndex = 5;
-            this.btnLogin.Text = "   登入";
+            this.btnLogin.Text = "   登录";
             this.btnLogin.UseVisualStyleBackColor = true;
             this.btnLogin.Click += new System.EventHandler(this.btnLogin_Click);
             // 
@@ -173,19 +173,32 @@
             this.btnExit.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
             this.btnExit.Image = ((System.Drawing.Image)(resources.GetObject("btnExit.Image")));
             this.btnExit.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.btnExit.Location = new System.Drawing.Point(267, 204);
+            this.btnExit.Location = new System.Drawing.Point(356, 255);
+            this.btnExit.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btnExit.Name = "btnExit";
-            this.btnExit.Size = new System.Drawing.Size(90, 32);
+            this.btnExit.Size = new System.Drawing.Size(120, 40);
             this.btnExit.TabIndex = 6;
             this.btnExit.Text = "  退出";
             this.btnExit.UseVisualStyleBackColor = true;
             this.btnExit.Click += new System.EventHandler(this.btnCancel_Click);
             // 
+            // lbStaffNo
+            // 
+            this.lbStaffNo.AutoSize = true;
+            this.lbStaffNo.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.lbStaffNo.Location = new System.Drawing.Point(55, 46);
+            this.lbStaffNo.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lbStaffNo.Name = "lbStaffNo";
+            this.lbStaffNo.Size = new System.Drawing.Size(52, 27);
+            this.lbStaffNo.TabIndex = 17;
+            this.lbStaffNo.Text = "工号";
+            this.lbStaffNo.Visible = false;
+            // 
             // MESLoginDialog
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(449, 245);
+            this.ClientSize = new System.Drawing.Size(599, 306);
             this.Controls.Add(this.btnExit);
             this.Controls.Add(this.btnLogin);
             this.Controls.Add(this.btnSaveIP);
@@ -202,10 +215,11 @@
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.KeyPreview = true;
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.MaximizeBox = false;
             this.Name = "MESLoginDialog";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "MES系统登入";
+            this.Text = "MES系统登录";
             this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.MESLoginDialog_KeyDown);
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -217,7 +231,6 @@
         private System.Windows.Forms.CheckBox checkBoxStaffnoLogin;
         private System.Windows.Forms.Label lbName;
         private System.Windows.Forms.TextBox txtBoxUserName;
-        private System.Windows.Forms.Label lbStaffNo;
         private System.Windows.Forms.TextBox txtStaffno;
         private System.Windows.Forms.Label pdcLbPswd;
         private System.Windows.Forms.TextBox txtBoxPassword;
@@ -227,5 +240,6 @@
         private System.Windows.Forms.Button btnSaveIP;
         private System.Windows.Forms.Button btnLogin;
         private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Label lbStaffNo;
     }
 }
