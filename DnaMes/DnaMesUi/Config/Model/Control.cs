e1500042d4829a5267aa493c4c44d2b4e12d966f
Charles Ma <charlesma.vip@gmail.com>
2018-02-20 09:53:34 +1100

    增加菜单设计模块

---
+++ DnaMes/DnaMesUi/Config/Model/Control.cs
@@ -0,0 +1,59 @@
+﻿// ****************************************************
+//  Author: Charles Ma
+//  Date: 2018/02/20 9:33
+// ****************************************************
+//  Copyright © DNA Studio 2018. All rights reserved.
+// ****************************************************
+
+using System.Collections.Generic;
+
+namespace DnaMesUi.Config.Model
+{
+    /// <summary>
+    /// 控件配置类
+    /// </summary>
+    public class Control
+    {
+        /// <summary>
+        /// 控件集合
+        /// </summary>
+        public List<ControlItem> Items { get; set; }
+    }
+    /// <summary>
+    /// 控件项基类
+    /// </summary>
+    public abstract class ControlItem
+    {
+        /// <summary>
+        /// 名字
+        /// </summary>
+        public string Name { get; set; }
+        /// <summary>
+        /// 显示名
+        /// </summary>
+        public string Text { get; set; }
+        /// <summary>
+        /// 停靠处
+        /// </summary>
+        public string Dock { get; set; }
+    }
+
+    public class Menu:ControlItem
+    {
+        
+    }
+
+    public abstract class MenuItem
+    {
+        /// <summary>
+        /// 名字
+        /// </summary>
+        public string Name { get; set; }
+        /// <summary>
+        /// 显示名
+        /// </summary>
+        public string Text { get; set; }
+        public string ShortCut { get; set; }
+    }
+}
+
