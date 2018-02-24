18eb06d62d59c29309b0583bb3e0af762a68b527
Charles Ma <charlesma.vip@gmail.com>
2018-02-24 12:02:16 +1100

    删除用户控件

--- DnaMes/DnaMesUi/BaseForm.Designer.cs
+++ DnaMes/DnaMesUi/BaseForm.Designer.cs
@@ -28,9 +28,17 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // BaseForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(868, 546);
+            this.Name = "BaseForm";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
